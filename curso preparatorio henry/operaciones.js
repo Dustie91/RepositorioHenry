a34f371ee69747be57fe6b4274cd33dc092b7031
dustie91 <daniela_gguzman@hotmail.com>
2024-05-31 00:57:31 -0500

    parte 1 java

---
+++ curso preparatorio henry/operaciones.js
@@ -0,0 +1,10 @@
+console.log(3 + 5);
+console.log(95 - 15);
+console.log(3 * 5);
+console.log(80 / 4);
+console.log(28 % 5);
+console.log((3 + 2) * (6 -3));
+var nombre = "Roberto";
+var apellido = "Lopez";
+var completo = nombre + " " + apellido;
+console.log(completo);
